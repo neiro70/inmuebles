ba8622034bb31497d2080b8daa15dabc597d5044
neiro70 <neiro70@gmail.com>
2018-09-24 17:09:18 -0500

    Pagina de construccion

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Bienvenido DrAtl</title>
+ </head>
+ <body>
+     <div align="center">
+        <img src="images/construccion.jpg" />
+        <br>
+        <img src="images/construction.gif" />
+    </div>
+ </body>
+</html>	
