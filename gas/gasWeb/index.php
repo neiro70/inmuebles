709e1d3c19f68c5c7c46386b1ee851270abe8f1c
Jorge Luis Martinez <neiro70@gmail.com>
2019-02-13 23:35:51 -0600

    p base

--- gas/gasWeb/index.php
+++ gas/gasWeb/index.php
@@ -14,7 +14,13 @@
 	    session_start();
 	  
 	}
-
+	$context= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	$var =explode("/",$context);
+	if (strpos($context, "localhost") !== false) {
+		$context="http://" .$var[0]."/".$var[1];
+	}else{
+		$context="http://" .$var[0];
+	}
 
 	include("mvc/util/MysqlDAO.php");
 
@@ -81,7 +87,7 @@
 			               				
 
 			               				
-			               				header("location:mvc/view/producto/viewBuscar.php");
+			               				header("location:{$context}/gas/gasWeb/mvc/view/producto/viewBuscar.php");
 			               			}else{
 			               				$msg = '&iexcl;Datos incorrectos!';
 			               			
