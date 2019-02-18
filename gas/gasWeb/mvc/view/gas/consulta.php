aa53880f165258d6bf6943b4f298878f14c3b406
Jorge Luis Martinez <neiro70@gmail.com>
2019-02-17 23:50:47 -0600

    subi consulta

--- gas/gasWeb/mvc/view/gas/consulta.php
+++ gas/gasWeb/mvc/view/gas/consulta.php
@@ -1,8 +1,9 @@
 <?php
+
 // Crear un nuevo post
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-
+  header('Access-Control-Allow-Origin: *');
   header('Content-Type: text/html; charset=UTF-8');
   include("../../../mvc/util/MysqlDAO.php");
 
