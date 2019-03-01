d3be548ee2e26f30f0a1e50f7f0233bde1a71c04
Jorge Luis Martinez <neiro70@gmail.com>
2019-02-28 22:13:02 -0600

    consulta para agendar

---
+++ gas/gasWeb/mvc/view/gas/consultaAgenda.php
@@ -0,0 +1,37 @@
+<?php
+
+// Crear un nuevo post
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{ 
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: text/html; charset=UTF-8');
+  include("../../../mvc/util/MysqlDAO.php");
+
+   
+    $crfc=trim($_GET['crfc']);
+
+    $db = new MySQL();   
+  
+    $sql="select * from t04agendar where idcliente  in( select idt03cliente  from t03cliente where rfc ='{$crfc}'); ";
+    
+    $conn=$db->getConexion();
+    
+    $result = $conn->query($sql);
+    $rows = array();
+   
+    if ($result->num_rows > 0) {//el cliente existe
+     // error_log("El cliente existe", 0);
+      while($row = $result->fetch_assoc()) {
+        $rows[]=$row;
+
+      }
+    }  
+    $db->closeSession();
+    
+
+      header("HTTP/1.1 200 OK");
+      echo json_encode($rows);
+      exit();
+     
+}
+?>
