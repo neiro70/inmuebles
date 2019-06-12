9c2ec5576e4e30cbc0cbeadcfadaf95fba37b5c9
neiro70 <neiro70@gmail.com>
2019-06-11 23:40:28 -0600

    upload files

---
+++ gas/gasWeb/mvc/view/gas/upload.php
@@ -0,0 +1,20 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+    $server =$_SERVER["HTTP_HOST"];
+	$contexto = "http://" . $server ."/inmuebles/gas/gasWeb/mvc/view/gas/";
+
+ if($_FILES['file']['name'] != ''){
+    $test = explode('.', $_FILES['file']['name']);
+    $extension = end($test);    
+    $name = rand(100,999).'.'.$extension;
+    $ABS_PATH='/opt/lampp/htdocs/inmuebles/gas/gasWeb/mvc/view/gas/';
+    $location = $ABS_PATH.$name;
+    move_uploaded_file($_FILES['file']['tmp_name'], $location);
+    chmod( $location, 0666);
+    
+    $imagen=$contexto.$name;
+
+    echo '<img src="'.$imagen.'" height="100" width="100" />';
+}
+
+?>
