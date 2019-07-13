9e051a6f32fd9df4e6dca0bc0ad612af156c9510
neiro70 <neiro70@gmail.com>
2019-07-13 15:16:34 -0500

    Consulta de usuario by rol

---
+++ gas/gasWeb/mvc/view/gas/consultaUsuarioByRol.php
@@ -0,0 +1,37 @@
+<?php
+
+// Crear un nuevo post
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{ 
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: text/html; charset=UTF-8');
+  include("../../../mvc/util/MysqlDAO.php");
+
+   
+    $idRol=trim($_GET['idRol']);
+
+    $db = new MySQL();   
+  
+    $sql="select * from t01usuario where idRol = ${idRol}";
+    
+    $conn=$db->getConexion();
+    
+    $result = $conn->query($sql);
+    $rows = array();
+   
+    if ($result->num_rows > 0) {//el cliente existe
+     // error_log("El cliente existe", 0);
+      while($row = $result->fetch_assoc()) {
+        $rows[]=$row;
+
+      }
+    }  
+    $db->closeSession();
+    
+
+      header("HTTP/1.1 200 OK");
+      echo json_encode($rows);
+      exit();
+     
+}
+?>
